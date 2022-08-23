7f4fb36b256dd0adb3c25a4cc3b38ed9e580a54b
zhu_ya_nan <wkqd159>
2022-08-23 09:57:24 +0800

    微信推送

---
+++ main.js
@@ -0,0 +1,148 @@
+const info = require('./modules/wxinfo.js')
+const static = require('./modules/icon.js')
+// 需要填写的地方已经标出
+// 一共4个地方 appID、appsecret、模板ID、微信用户的openid
+const axios = require('axios');
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+
+
+const axiosPost = function (url, params) {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(url, params)
+      .then(res => {
+        resolve(res);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
+const axiosGet = function (url, params) {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url, {
+        params,
+      })
+      .then(res => {
+        resolve(res);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
+
+async function getToken() {
+  const params = {
+    grant_type: 'client_credential',
+   appid: 'wxfa719149d39f9f76', // 你的appid  1
+   secret: 'a43325f1391f463c8750e17c094773a2', // 你的secret 2
+  };
+  let res = await axiosGet('https://api.weixin.qq.com/cgi-bin/token', params);
+  return res.data.access_token;
+}
+
+// 今天的日期
+function getNowDate(){
+	return info.nowDate()
+}
+// 时间差
+function getDate(date){
+	return info.date(date)
+}
+// 天气预报
+function getweather(){
+	return new Promise((resolve,reject)=>{
+		info.weather('南京市')
+			.then(res=>{
+				resolve(res.data)
+			})
+	})
+	
+}
+// 每日金句
+function getSaying(){
+	return new Promise((resolve,reject)=>{
+		info.saying()
+			.then(res=>{
+				resolve(res.data)
+			})
+	})
+	
+}
+// 天气图标
+function getIcon(weather_type){
+	return new Promise((resolve,reject)=>{
+		resolve(info.weatherIcon(weather_type))
+	})
+}
+// 0-2的随机数
+function getNum(){
+	return new Promise((resolve,reject)=>{
+		resolve(info.randomColor(0,2))
+	})
+}
+
+async function templateMessageSend() {
+  const date = await getNowDate()
+  const loveday = await getDate(date)
+  const weather = await getweather()
+		const love_mesg = weather.data.ganmao
+		const weather_type = weather.data.forecast[0].type
+		const min_temperature = weather.data.forecast[0].low
+		const max_temperature = weather.data.forecast[0].high  
+  const saying = await getSaying()
+  const icon = await getIcon(weather_type)
+  const num = await getNum()
+  // console.log('num===',num)
+  
+  const colorarry = static.color[num]
+  // console.log('colorarry===',colorarry)
+  
+  const token = await getToken();
+  const url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=' + token;
+  const params = {
+   touser: 'o5Qb46a7Vim04_vkfFVprHzIYL7w', // 用户openid 3dds
+   template_id: 'nsPkSagQKgAVBgw6k03pKGjK-OfF330cVUQfiT0Lhyk', // 模板id 4  sssssssssssssssss 
+   
+    url: 'http://www.baidu.com',
+    data: {
+	  date: {
+	    value: date,
+	    color: colorarry.cl1,
+	  },
+	  weather: {
+	    value: weather_type + icon,
+	    color: colorarry.cl2,
+	  },
+	  min_temperature: {
+	    value: min_temperature,
+	    color: colorarry.cl3,
+	  },
+	  max_temperature: {
+	    value: max_temperature,
+	    color: colorarry.cl4,
+	  },
+	  love_mesg: {
+	    value: love_mesg,
+	    color: colorarry.cl5,
+	  },
+	  love_day: {
+	    value: loveday,
+	    color: colorarry.cl1,
+	  },
+	  saying: {
+	    value: 'ღ'+ saying + 'ღ',
+	    color: colorarry.cl6,
+	  },
+    },
+  };
+  let res = await axiosPost(url, params);
+  // console.log('res: ', res);s
+}
+setInterval(function(){
+	console.log('发送了')
+	templateMessageSend();
+},3000)
---
+++ modules/wxinfo.js
@@ -0,0 +1,88 @@
+const axios = require('axios');
+const icon = require('./icon.js')
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+module.exports = {
+	//获取当前日期函数
+	nowDate:function() {
+	  let date = new Date(),
+	    seperator1 = '-', //格式分隔符
+	    year = date.getFullYear(), //获取完整的年份(4位)
+	    month = date.getMonth() + 1, //获取当前月份(0-11,0代表1月)
+	    strDate = date.getDate() // 获取当前日(1-31)
+	  if (month >= 1 && month <= 9) month = '0' + month // 如果月份是个位数，在前面补0
+	  if (strDate >= 0 && strDate <= 9) strDate = '0' + strDate // 如果日是个位数，在前面补0
+	 
+	  let currentdate = year + seperator1 + month + seperator1 + strDate 
+	  return currentdate
+	},
+	// 时间差
+	date:function(nowdate){
+		var date1 = nowdate;
+		var stime = new Date(date1).getTime();
+		var date2 = "2021-01-06";
+		var etime = new Date(date2).getTime();
+		// console.log(stime);//大的时间
+		// console.log(etime);//小的时间
+		let usedTime = stime - etime; // 两个时间戳相隔的毫秒数
+		let days = Math.floor(usedTime / (24 * 3600 * 1000)); // 计算相差的天数
+		// console.log(days)//相差的时间
+		return days
+	},
+	weather:function(city){
+		const BASE_url = 'http://wthrcdn.etouch.cn/weather_mini?city='
+		var city = encodeURI(city)
+		return new Promise((resolve, reject) => {
+			axios.get(BASE_url + city)
+				.then(res => {	
+					resolve(res);
+				})
+				.catch(err => {
+					reject(err);
+				});
+		});
+	},
+	saying:function(){
+		return new Promise((resolve, reject) => {
+			axios.get('https://api.mcloc.cn/love/')
+				.then(res => {		
+					resolve(res);
+				})
+				.catch(err => {
+					reject(err);
+				});
+			});
+	},
+	// 天气图标
+	weatherIcon:function(type){
+		let typeicon;
+		switch(type)
+		{
+			case '晴' : typeicon = icon.wether[1]
+			break
+			case '阴' : typeicon = icon.wether[0]
+			break
+			case '多云' : typeicon = icon.wether[2]
+			break
+			case '小雨' : typeicon = icon.wether[3]
+			break
+			case '中雨' : typeicon = icon.wether[3]
+			break
+			case '大雨' : typeicon = icon.wether[3]
+			break
+			case '小雪' : typeicon = icon.wether[7]
+			break
+			case '中雪' : typeicon = icon.wether[8]
+			break
+			case '大雪' : typeicon = icon.wether[8]
+			break
+		}
+		return typeicon
+	},
+	// 随机颜色  Min <= num <= Max
+	randomColor:function(Min,Max){
+		var Range = Max - Min;
+		var Rand = Math.random();
+		var num = Min + Math.round(Rand * Range); //四舍五入
+		return num;
+	}
+}
